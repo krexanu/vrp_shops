f0e4df45df0f51686063ec5b77ef0c0fd29a6e0b
krexanu <44748244+krexanu@users.noreply.github.com>
2024-08-08 14:25:06 +0300

    Update shops.lua

--- client/shops.lua
+++ client/shops.lua
@@ -1,26 +1,4 @@
 shopTable = {}
-local Config = {
-    pedDensity = 0,
-    scenarioPedDensity = 0,
-    vehicleDensity = 0,              -- Max value 1.0
-    randomVehicleDensity = 0,
-    parkedVehicleDensity = 0
-}
-
-Citizen.CreateThread(function()
-    while true do
-        -- Réglage de la densité des PNJ / Adjusting NPC density
-        SetPedDensityMultiplierThisFrame(Config.pedDensity)
-        SetScenarioPedDensityMultiplierThisFrame(Config.scenarioPedDensity, Config.scenarioPedDensity)
-
-        -- Réglage de la densité de la circulation / Adjusting traffic density
-        SetVehicleDensityMultiplierThisFrame(Config.vehicleDensity)
-        SetRandomVehicleDensityMultiplierThisFrame(Config.randomVehicleDensity)
-        SetParkedVehicleDensityMultiplierThisFrame(Config.parkedVehicleDensity)
-        
-        Wait(1)
-    end
-end)
 function tvRP.reset()
     shopTable = nil
 end
